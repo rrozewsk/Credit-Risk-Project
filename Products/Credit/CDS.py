3e7681257e446d1914c79af1b14e7836001a817a
rrozewsk <jimbo@Jims-MacBook-Pro.local>
2016-10-18 14:50:49 -0400

    added python script professor gave us

--- Products/Credit/CDS.py
+++ Products/Credit/CDS.py
@@ -1,11 +1,12 @@
 from parameters import freq, Coupon
 __author__ = 'ryan rozewski'
 class CDS(object):
-    def __init__(self,start,maturity,freq,coupon,refDate):
+    def __init__(self,start,maturity,freq,coupon,refDate,Q):
         self.startDate=start
         self.end=maturity
         self.freq=freq
         self.coupon=coupon
         self.refDate=refDate
+        self.q=Q
 
     def
--- Products/Rates/Bond.py
+++ Products/Rates/Bond.py
@@ -4,6 +4,7 @@ from pandas import DataFrame
 from Scheduler import Scheduler
 from parameters import xR,simNumber,trim_start,trim_end,t_step
 from MonteCarloSimulators.Vasicek.vasicekMCSim import MC_Vasicek_Sim
+from scipy.optimize import minimize
 
 
 
@@ -43,12 +44,19 @@ class Bond(object):
         return self.pvAvg
     def getYield(self):
         pass
+    
+    def calibrateCurve(self,fcurve,x):
+        fSum=np.sum(fcurve)
+        xSum=np.sum(x)
+        error=0
+        error=minimize((fSum-xSum)**2)
+        return error
 
 
 myScheduler=Scheduler.Scheduler()
 datelist=myScheduler.getSchedule(start=trim_start, end=trim_end, freq="3M")
 
 coupon=.03
-libor=MC_Vasicek_Sim(x=xR,simNumber=simNumber,t_step=t_step,datelist=datelist)
-myBond=Bond(start=trim_start,maturity=trim_end,coupon=coupon,frequency='3M',reference_date=trim_start,libor=libor.getSmallLibor(x=xR,tenors=datelist,simNumber=simNumber))
+libor=MC_Vasicek_Sim(x=xR,simNumber=500,t_step=t_step,datelist=datelist)
+myBond=Bond(start=trim_start,maturity=trim_end,coupon=coupon,frequency='3M',reference_date=trim_start,libor=libor.getSmallLibor(x=xR,tenors=datelist,simNumber=500))
 print(myBond.PV())
